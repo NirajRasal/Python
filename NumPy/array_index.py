18d43c41b28808051995961dc9ceb3edf093542c
NirajRasal <48594267+NirajRasal@users.noreply.github.com>
2020-11-11 19:58:41 +0530

    NumPy module

---
+++ NumPy/array_index.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a = np.array([1,2,3,4])
+
+print(a[0])
+print(a[0] + a[2])  #addition of 1 and 3
---
+++ NumPy/testing_numpy.py
@@ -0,0 +1,9 @@
+import numpy as np          #np as alias to numpy
+
+a = np.array([1,2,3,4])                     # 1-D array
+b = np.array([[1, 2, 3], [4, 5, 6]])        #2-D array
+
+print(a)
+print(type(a))      # ndarray using numpy
+
+print(b.ndim)       #array dimension
